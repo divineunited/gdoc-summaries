357bddc63b14d590b6aa96a52a5e6a42dee4fdca
Danny Vu <danieldukevu@outlook.com>
2023-11-29 23:50:01 +0800

    implement Drive and Docs API

--- gdoc_summaries/run.py
+++ gdoc_summaries/run.py
@@ -1,41 +1,208 @@
+"""Main entrypoint for script to run Gdoc Summaries"""
+
+import logging
+import os.path
+from datetime import datetime, timedelta
+
+from google import auth
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-creds = None
-# Load your credentials from the 'token.json' file
-creds = Credentials.from_authorized_user_file("token.json")
+LOGGER = logging.getLogger(__name__)
 
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    flow = InstalledAppFlow.from_client_secrets_file("your_credentials.json", SCOPES)
-    creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-        token.write(creds.to_json())
-
-# Call the Docs API
-service = build("docs", "v1", credentials=creds)
-document = service.documents().get(documentId=document_id).execute()
-
-# Assume the structure of the document is as expected
-# (a Heading 2 followed by a Table)
-header = document["body"]["content"][1]["paragraph"]["elements"][0]["textRun"][
-    "content"
+# This script needs scope access to the Docs and Drive and Email APIs
+SCOPES = [
+    "https://www.googleapis.com/auth/documents.readonly",
+    "https://www.googleapis.com/auth/drive.metadata.readonly",
+    # TODO: Add email scope.
 ]
-table = document["body"]["content"][2]["table"]
 
-# Extract signoff details from the table
-for row in table["tableRows"]:
-    cells = row["tableCells"]
-    name = cells[0]["content"][0]["paragraph"]["elements"][0]["textRun"]["content"]
-    signoff = cells[2]["content"][0]["paragraph"]["elements"][0]["textRun"]["content"]
+# TODO DEPLOYMENT: This credential is my Danny Dev sandbox in the Clover eng sandbox
+# it only has access to docs that I give it access to
+# Clover TDDs only are able to give access to users within CloverHealth
+# so I created a TDD within my own drive, and shared it to the service account email
+# which is: danny-dev@eng-sandbox.iam.gserviceaccount.com
+# The drive API goes through all docs that the service account has access to
+# You can just grant access for each TDD that we want this to chase for sign off.
+CREDS_PATH = os.path.expanduser("~/Downloads/eng-sandbox-30f6bd0e093d.json")
 
-    # If the signoff cell is empty, send an email
-    if not signoff.strip():
-        email_address = name.split("@")[1]
-        send_email(email_address, document["title"], document["documentId"])
+
+def get_credentials(creds_path: str, scopes: list[str]) -> Credentials:
+    """Get Google API Credentials"""
+    creds, _ = auth.load_credentials_from_file(creds_path, scopes=scopes)
+
+    # If the credentials are invalid (e.g., expired), refresh them
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+    return creds
+
+
+def get_doc_ids_from_drive(creds: Credentials) -> list[str]:
+    """
+    This function gets all relevant Google Docs that it has access to
+    from the Google Drive API
+    """
+    # Get the date 30 days ago
+    past_date = datetime.now() - timedelta(days=30)
+    past_date_str = (
+        past_date.strftime("%Y-%m-%dT%H:%M:%S") + "Z"
+    )  # Format it as RFC 3339 timestamp
+
+    try:
+        # Call the Drive v3 API
+        service = build("drive", "v3", credentials=creds)
+        # Query for Google Docs modified or created in the last 30 days
+        # -- change to createdTime if modified is too noisy
+        # -- remove the q object if you dont want to filter at all
+        results: dict = (
+            service.files()
+            .list(
+                q=f"mimeType='application/vnd.google-apps.document' and modifiedTime >= '{past_date_str}'",
+                pageSize=10,
+                fields="nextPageToken, files(id, name)",
+            )
+            .execute()
+        )
+        items: list[dict] = results.get("files", [])
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        raise
+
+    if not items:
+        LOGGER.error("No files found.")
+        return []
+
+    doc_ids = [item["id"] for item in items]
+    # doc_names = [item["name"] for item in items]
+    # print(f"File IDs: {doc_ids}")
+    # print(f"File Doc Names: {doc_names}")
+
+    return doc_ids
+
+
+def get_doc_from_id(creds: Credentials, document_id: str) -> dict:
+    """
+    Use the Google Docs API to return the GDoc as a dict
+    given the Doc ID
+    """
+    try:
+        # Retrieve the documents contents from the Docs service.
+        service = build("docs", "v1", credentials=creds)
+        document = service.documents().get(documentId=document_id).execute()
+    except HttpError as err:
+        print(err)
+        raise
+
+    print(f"---NEXT DOC: {document.get('title')}---")
+    return document
+
+
+def find_signoff_table(doc_contents: dict) -> dict | None:
+    """
+    Given a the content of a GDoc represented as a Python Dict
+    Find the index where the SignOff table would be stored.
+    """
+    table_index = None
+    # Find the index of the sign off table:
+    for i, content in enumerate(doc_contents):
+        header_content = (
+            content.get("paragraph", {})
+            .get("elements", [{}])[0]
+            .get("textRun", {})
+            .get("content", "")
+        )
+
+        if not header_content:
+            continue
+
+        # If the header portion has the text "Signoff"
+        if header_content.strip().lower() in ("signoff", "sign-off"):
+            # then the signoff table is the next content right below it.
+            table_index = i + 1
+            break
+    if table_index is not None:
+        try:
+            # make sure the table exists:
+            table = doc_contents[table_index]["table"]
+            return table
+        except KeyError:
+            return None
+
+
+def find_email_and_signoff_from_row(cells: list[str]) -> tuple[str, str]:
+    """Extracts the email and signoff from a row of cells in a GDoc
+
+    The table must be formatted such with 3 columns in the @NAME | RACI | DATE format
+    Example: @DannyVu | R | 2023-12-25
+    """
+    # wrap this to defend against malformed tables
+    try:
+        email = (
+            cells[0]
+            .get("content", [{}])[0]
+            .get("paragraph", {})
+            .get("elements", [{}])[0]
+            .get("person", {})
+            .get("personProperties", {})
+            .get("email", "")
+        ).strip()
+
+        # Check for sign off in the 3rd column
+        signoff = (
+            cells[2]
+            .get("content", [{}])[0]
+            .get("paragraph", {})
+            .get("elements", [{}])[0]
+            .get("textRun", {})
+            .get("content", "")
+        ).strip()
+    except IndexError:
+        print("Malformed table: no email / signoff found.")
+        return "", ""
+
+    return email, signoff
+
+
+def entrypoint() -> None:
+    """Entrypoint for GDoc Summaries"""
+
+    # Get the Creds and potential Google Doc IDs
+    creds = get_credentials(creds_path=CREDS_PATH, scopes=SCOPES)
+    doc_ids = get_doc_ids_from_drive(creds)
+
+    # Do the work for each GDoc
+    for doc_id in doc_ids:
+        document = get_doc_from_id(creds, doc_id)
+        contents = document.get("body", {}).get("content", [])
+        if not contents:
+            continue
+
+        table = find_signoff_table(contents)
+        if table is None:
+            print(f"No signoff table was found for Doc ID: {doc_id}")
+            continue
+
+        # Extract signoff details from the table
+        for row in table["tableRows"]:
+            cells: list[dict] = row["tableCells"]
+            email, signoff = find_email_and_signoff_from_row(cells=cells)
+            if not email:
+                continue
+            if not signoff:  # not signed off, send email to remind for signoff.
+                # TODO: Get Executive Summary (And possible NLP summary) to send along with email
+                send_email(email, document["title"], document["documentId"])
 
 
 def send_email(to, doc_name, doc_id):
-    # Use the Gmail API to send an email
-    pass
+    """Use the Gmail API to send the email to the user"""
+    # TODO send the email
+    print(f"Sending e-mail to: {to}")
+    print(to, doc_name, doc_id)
+
+
+if __name__ == "__main__":
+    entrypoint()
